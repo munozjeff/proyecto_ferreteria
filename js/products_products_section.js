0c3b0ac62f4d9ec0f66b7bf617e5f6e92caeac03
jefferson <jeffersonsteven231997@gmail.com>
2023-02-09 19:41:25 -0500

    servicios

---
+++ js/products_products_section.js
@@ -0,0 +1,29 @@
+const container = document.getElementById("product_section")
+
+for(let i=0;i<=10;i++){
+    add_item()
+}
+
+
+function add_item(){
+    let item = document.createElement('div');
+    item.classList.add('item')
+    let figure = document.createElement('figure');
+    figure.classList.add('figure_item')
+    let img = document.createElement('img')
+    img.classList.add('img_item')
+    let info_1 = document.createElement('span')
+    info_1.classList.add('info_item')
+    let info_2 = document.createElement('span')
+    info_1.classList.add('info_item')
+
+    img.src = ('../img/alicate.png')
+    info_1.textContent = ('PRODUCT NAME')
+    info_2.textContent = ('PRODUCT INFO')
+
+    figure.appendChild(img)
+    item.appendChild(figure)
+    item.appendChild(info_1)
+    item.appendChild(info_2)
+    container.appendChild(item)
+}
